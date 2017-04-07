9f45802472aa202eb080eca00586c3a63d8ba01f
YJSoft <YJSoft@users.noreply.github.com>
2017-04-07 09:57:32 +0900

    add document_srl url redirect

--- redirector/background.js
+++ redirector/background.js
@@ -1,9 +1,11 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-        var pattern,indexphp, middocumentsrl_from, middocumentsrl_to, mid_from, mid_to, redirectUrl, match, xefolder;
+        var pattern,indexphp,documentsrl_from,documentsrl_to, middocumentsrl_from, middocumentsrl_to, mid_from, mid_to, redirectUrl, match, xefolder;
 
         //setting regex
         indexphp = "^http:\/\/f-planet\.co\.kr\/index\.php(.*)";
+        documentsrl_from = "^http:\/\/f-planet\.co\.kr\/([0-9]+)(\/?)";
+        documentsrl_to = "http://f-planet.co.kr/index.php?document_srl=$1";
         middocumentsrl_from = "^http:\/\/f-planet\.co\.kr\/([^\/]+)\/([^\/]+)";
         middocumentsrl_to = "http://f-planet.co.kr/index.php?mid=$1&document_srl=$2";
         mid_from = "^http:\/\/f-planet\.co\.kr\/([^\/]+)(\/?)";
@@ -12,6 +14,7 @@ chrome.webRequest.onBeforeRequest.addListener(
 
         // compile regex
         indexphp = new RegExp(indexphp, 'ig');
+        documentsrl_from = new RegExp(documentsrl_from, 'ig');
         middocumentsrl_from = new RegExp(middocumentsrl_from, 'ig');
         mid_from = new RegExp(mid_from, 'ig');
         xefolder = new RegExp(xefolder, 'ig');
@@ -30,7 +33,7 @@ chrome.webRequest.onBeforeRequest.addListener(
             return {};
         }
 
-        //if url format is mid/document_srl
+        //if url format is /mid/document_srl
         match = details.url.match(middocumentsrl_from);
         if (match) {
             redirectUrl = details.url.replace(middocumentsrl_from, middocumentsrl_to);
@@ -39,7 +42,16 @@ chrome.webRequest.onBeforeRequest.addListener(
             }
         }
 
-        //if url format is mid
+        //if url format is /document_srl
+        match = details.url.match(documentsrl_from);
+        if (match) {
+            redirectUrl = details.url.replace(documentsrl_from, documentsrl_to);
+            if (redirectUrl != details.url) {
+                return {redirectUrl: redirectUrl};
+            }
+        }
+
+        //if url format is /mid
         match = details.url.match(mid_from);
         if (match) {
             redirectUrl = details.url.replace(mid_from, mid_to);
